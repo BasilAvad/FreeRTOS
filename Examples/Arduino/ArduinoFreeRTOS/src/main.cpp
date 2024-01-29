95e18f76a563124e298a76c5565dc1f2b1d1295c
BASİL AVAD <69681817+BasilAvad@users.noreply.github.com>
2024-01-29 21:57:31 +0300

    Read analog input using RTOS analoagReadTask

--- Examples/Arduino/ArduinoFreeRTOS/src/main.cpp
+++ Examples/Arduino/ArduinoFreeRTOS/src/main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
-#define StackSize 64 
-
+#define StackSize 64
+float adc = 0.0f;
 void analogReadTask(void *pvParameter);
 void analogWriteTask(void *pvParameter);
 void setup()
 {
   Serial.begin(9600);
+  pinMode(A0, INPUT);
   xTaskCreate(analogReadTask, "analogReadTask", StackSize, NULL, 1, NULL);
   xTaskCreate(analogWriteTask, "analogWriteTask", StackSize, NULL, 2, NULL);
 }
@@ -19,6 +20,7 @@ void analogReadTask(void *pvParameter)
   for (;;)
   {
     Serial.println("Task analogReadTask is running");
+    adc = analogRead(A0);
     vTaskDelay(20 / portTICK_PERIOD_MS);
   }
 }
