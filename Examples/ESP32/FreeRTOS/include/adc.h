4c9fc91527ec1faeb68371bdce55b9089f25ed1c
BASİL AVAD <69681817+BasilAvad@users.noreply.github.com>
2024-01-31 22:21:43 +0300

    Optimize the ADC library

--- Examples/ESP32/FreeRTOS/include/adc.h
+++ Examples/ESP32/FreeRTOS/include/adc.h
@@ -1,18 +1,30 @@
 #ifndef H_ADC_L
 #define H_ADC_L
+
 // User Includes
 #include <driver/adc.h>
 #include <driver/gpio.h>
 #include <driver/dac.h>
-//
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 class adc
 {
 private:
-    /* data */
+    // Private data members can be added here if needed
+
 public:
-    adc(/* args */);
+    adc();          // Constructor
+    ~adc();         // Destructor
     void adc_dma_Init();
     float adc_read(int channel);
-    ~adc();
 };
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
 #endif
