326504c00206b7f19f26889165f74a861357eb13
BASİL AVAD <69681817+BasilAvad@users.noreply.github.com>
2024-02-01 15:46:06 +0300

    Change the functions name and config the adc resolution as 12 bit

--- Examples/ESP32/FreeRTOS/include/adc.h
+++ Examples/ESP32/FreeRTOS/include/adc.h
@@ -1,10 +1,10 @@
 #ifndef H_ADC_L
 #define H_ADC_L
-
 // User Includes
 #include <driver/adc.h>
 #include <driver/gpio.h>
 #include <driver/dac.h>
+
 
 #ifdef __cplusplus
 extern "C"
@@ -14,12 +14,11 @@ extern "C"
 class adc
 {
 private:
-
 public:
-    adc();          
-    ~adc();         
-    void adc_dma_Init();
-    float adc_read(int channel);
+    adc();
+    ~adc();
+    void adc_Init();
+    float adc_read(float *_data);
 };
 
 #ifdef __cplusplus
--- Examples/ESP32/FreeRTOS/src/adc.cpp
+++ Examples/ESP32/FreeRTOS/src/adc.cpp
@@ -1,12 +1,15 @@
 #include <adc.h>
 
-float adc::adc_read(int channel)
-{
-}
-void adc::adc_dma_Init()
+#define channel_1 37 /*!< ADC1 channel 1 is GPIO37 */
+#define ADC_RESOLUTION ADC_WIDTH_12Bit
+
+void adc::adc_Init()
 {
 }
 
+float adc::adc_read(float *_data)
+{
+}
 adc::~adc()
 {
 }
