eeef7fd06722dcff633ccadb7c668918d8c43586
BASİL AVAD <69681817+BasilAvad@users.noreply.github.com>
2024-02-01 15:47:47 +0300

    Configure ADC1 for reading analog data

--- Examples/ESP32/FreeRTOS/src/adc.cpp
+++ Examples/ESP32/FreeRTOS/src/adc.cpp
@@ -5,10 +5,14 @@
 
 void adc::adc_Init()
 {
+    adc1_config_width(ADC_RESOLUTION);
+    adc1_config_channel_atten(ADC1_CHANNEL_1, ADC_ATTEN_11db);
 }
 
 float adc::adc_read(float *_data)
 {
+    _data[0] = adc1_get_raw(ADC1_CHANNEL_1);
+    return 0;
 }
 adc::~adc()
 {
